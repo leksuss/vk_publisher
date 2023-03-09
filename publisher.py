1204e22fd53e2c85f7c34d29e68c4a8a00f9032d
Andrey Alekseev <leksus@leksus.info>
2023-03-09 11:06:19 +0300

    refactor: naming, unpacking params

--- publisher.py
+++ publisher.py
@@ -19,7 +19,7 @@ def fetch_vk_upload_server_url(group_id, api_key):
     return response.json()['response']['upload_url']
 
 
-def upload_img_to_vk_server(upload_url, filepath, api_key):
+def upload_img_to_vk_server(upload_url, filepath):
 
     with open(filepath, 'rb') as file:
         files = {
@@ -31,15 +31,15 @@ def upload_img_to_vk_server(upload_url, filepath, api_key):
     return response.json()
 
 
-def save_img_to_vk_server(group_id, upload_response, api_key):
+def save_img_to_vk_server(group_id, photo, server, hash, api_key):
 
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
 
     params = {
         'group_id': group_id,
-        'photo': upload_response['photo'],
-        'server': upload_response['server'],
-        'hash': upload_response['hash'],
+        'photo': photo,
+        'server': server,
+        'hash': hash,
         'access_token': api_key,
         'v': VK_API_VERSION,
     }
@@ -49,7 +49,7 @@ def save_img_to_vk_server(group_id, upload_response, api_key):
     return response.json()['response']
 
 
-def posting_img_to_vk_wall(group_id, owner_id, media_id, message, api_key):
+def post_img_to_vk_wall(group_id, owner_id, media_id, message, api_key):
 
     url = 'https://api.vk.com/method/wall.post'
 
@@ -74,12 +74,17 @@ def publish_post(group_id, img_text, img_filepath, api_key):
     upload_response = upload_img_to_vk_server(
         vk_upload_server_url,
         img_filepath,
-        api_key,
     )
 
-    saved_pic = save_img_to_vk_server(group_id, upload_response, api_key)
+    saved_pic = save_img_to_vk_server(
+        group_id,
+        upload_response['photo'],
+        upload_response['server'],
+        upload_response['hash'],
+        api_key
+    )
 
-    publish_status = posting_img_to_vk_wall(
+    publish_status = post_img_to_vk_wall(
         group_id,
         saved_pic[0]['owner_id'],
         saved_pic[0]['id'],
